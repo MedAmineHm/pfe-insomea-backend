068d7de9fd9d189ae477f245eb1ddbaaa4b62fa3
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-06-07 01:57:25 +0100

    hamidou

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors'; // Importez le module cors
 
 // Charger les variables d'environnement à partir de backend.env
 dotenv.config({ path: 'backend.env' });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  // Activer CORS avec les options par défaut
+  app.use(cors());
 
   await app.listen(3001);
 }
