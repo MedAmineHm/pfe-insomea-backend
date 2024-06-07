a7ffb6d1cf52dc5b50900771e4695c3efd386050
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-06-07 14:29:10 +0100

    ippppp

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 // Charger les variables d'environnement à partir de backend.env
+dotenv.config({ path: 'backend.env' });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   await app.listen(3001);
 }
