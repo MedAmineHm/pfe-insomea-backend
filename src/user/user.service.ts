957db3347ff05d84d162e53bf15669794984d94f
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-06-07 15:17:31 +0100

    deee

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,17 +4,18 @@ import { Model } from 'mongoose';
 import { User } from 'src/types/user';
 import { RegisterDTO, RegisterGoogleDTO } from './dtos/register.dto';
 import { LoginDTO } from './dtos/login.dto';
-import * as bcrypt from 'bcryptjs'; // Remplacez bcrypt par bcryptjs
+import * as bcrypt from 'bcryptjs';
 import { Payload } from 'src/types/payload';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private userModel: Model<User>) {}
 
+  // Create new user
   async create(registerDTO: RegisterDTO) {
     const { email, password } = registerDTO;
-    const existingUser = await this.userModel.findOne({ email });
-    if (existingUser) {
+    const user = await this.userModel.findOne({ email });
+    if (user) {
       throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
     }
 
@@ -22,31 +23,29 @@ export class UserService {
       throw new HttpException('Password is required', HttpStatus.BAD_REQUEST);
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10); // Utilisez bcryptjs pour hacher le mot de passe
-    const createUser = new this.userModel({
-      ...registerDTO,
-      password: hashedPassword,
-    });
+    const createUser = new this.userModel(registerDTO);
     await createUser.save();
     return this.sanitizeUser(createUser);
   }
 
+  // Find by payload
   async findByPayload(payload: Payload) {
     const { email } = payload;
     return await this.userModel.findOne({ email });
   }
 
+  // Find by login
   async findByLogin(UserDTO: LoginDTO) {
     const { email, password } = UserDTO;
     const user = await this.userModel.findOne({ email });
     if (!user) {
-      throw new HttpException("User doesn't exist", HttpStatus.BAD_REQUEST);
+      throw new HttpException('user doesnt exists', HttpStatus.BAD_REQUEST);
     }
     if (await bcrypt.compare(password.toString(), user.password)) {
       return this.sanitizeUser(user);
     } else {
       console.log({ user });
-      throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+      throw new HttpException('invalid credential', HttpStatus.BAD_REQUEST);
     }
   }
 
@@ -56,12 +55,12 @@ export class UserService {
     return this.sanitizeUser(createUser);
   }
 
+  // return user object without password
   sanitizeUser(user: User) {
     const sanitized = user.toObject();
     delete sanitized['password'];
     return sanitized;
   }
-
   async findByEmail(email: string): Promise<User | null> {
     return this.userModel.findOne({ email }).exec();
   }
