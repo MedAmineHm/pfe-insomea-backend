2238a19c60044ccd0a49229f887465329232e3e5
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-07-26 13:42:25 +0100

    wwwwwwwwwwwwww

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://172.179.168.160:3000', 'http://172.179.168.160:3000'],
+    origin: '*', // Allow requests from any origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true, // Include credentials in requests
   });
   await app.listen(3001);
 }
