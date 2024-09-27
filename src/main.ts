222194ccdb2d31e3abd34e482066af75f8ee9d47
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-09-27 23:12:37 +0100

    zzzzzz

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors'; // Change to default import
+import cors from 'cors'; // Change to default importaaaaa
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
